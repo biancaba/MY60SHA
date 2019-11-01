12cb00823f4b4f38148bb1e4f99cb1bdab05978e
Bianca Bunaciu <bianca.bunaciu@citrix.com>
2019-11-01 15:33:52 +0000

    Implementation of MY60SHA using the previous hash to generate a new hash

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import hashlib
 import random
 import string
 
-NIBBLES = 6
+NIBBLES = 8
 HASHTABLE = {}
 
 def my60sha(string, nibbles):
@@ -27,7 +27,27 @@ def find_collision_random():
         # if no collision found add to hash table
         HASHTABLE[s] = h
 
-result = find_collision_random()
+def find_collision_recursive(initial_string):
+	# Start with an initial string
+	initial_hash = my60sha(initial_string, NIBBLES)
+	HASHTABLE[initial_string] = initial_hash
+
+	# Use the hash as a random string
+	while(True):
+		s = initial_hash
+		h = my60sha(s, NIBBLES)
+
+        # check for collision
+		for k, v in HASHTABLE.items():
+			if v == h and k != s: 
+				return k, s
+        
+        # if no collision found add to hash table
+		HASHTABLE[s] = h
+		initial_hash = h
+
+
+result = find_collision_recursive(randomStringDigits())
 print (result)
 
 print(len(HASHTABLE.keys()))
